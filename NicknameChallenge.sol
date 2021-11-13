94b75cd593a888c338708f64a3f21ae495458b65
josealonso <jose.alonso.programmer@gmail.com>
2021-11-13 19:35:58 +0100

    Explained with code comments how the Nickname Challenge has been checked.

--- NicknameChallenge.sol
+++ NicknameChallenge.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.7;
 
@@ -32,22 +32,17 @@ contract Attacker {
     address constant ADDRESSS_WHERE_CAPTURETHEETHER_IS_DEPLOYED = 0x71c46Ed333C35e4E6c62D32dc7C8F00D125b4fee;
     CaptureTheEther c = CaptureTheEther(ADDRESSS_WHERE_CAPTURETHEETHER_IS_DEPLOYED);
 
-/*    function setNumber(uint256 number) public {
-        c.setNumber(number);
-    } */
-
     function setNickname(string memory s) public {
         // conversion from string/bytes to bytes32
         c.setNickname(bytes32(bytes(s))); 
     }
     
-    function setString(string memory s2) public {
-        // c.setString(bytes32(bytes(s2)));
-    }
 }
 
 /*
- I can check here https://ropsten.etherscan.io/tx/0x32bc55b78005d5a50d0c245b5e29f165639b7feef23f4bfcc1ee8fc09f5b1746 the conversion from string to bytes32 is correct. 
-But the state is not modified !!
+ 1.- CaptureTheEther is deployed at 0x71c46Ed333C35e4E6c62D32dc7C8F00D125b4fee
+ 2.- NicknameChallenge is deployed at 0xB1FEcaBFC2E9e3DF4095c34eDcC304DBc25aaF50
+ 3.- I deploy the Attacker smart contract.
+ 4.- I execute the setNickname function from the Attacker contract.
+ 5.- Read the state variable nicknameOf[adr], being adr the address where the Attacker contract has been deployed.
 */
-
