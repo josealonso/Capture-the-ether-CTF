2af23bcb51f14423c85fc12d6af97bb30729b575
josealonso <jose.alonso.programmer@gmail.com>
2021-11-13 23:57:05 +0100

    Modified the GuessTheNumber challenge and adapted to Solidity version 0.8

--- GuessTheNumberChallenge.sol
+++ GuessTheNumberChallenge.sol
@@ -1,10 +1,13 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 contract GuessTheNumberChallenge {
-    uint8 answer = 42;
+    
+    uint8 constant ANSWER = 42;
 
     constructor() payable {
-        require(msg.value == 1 ether);
+        require(msg.value == 100000000 gwei);
     }
 
     function isComplete() public view returns (bool) {
@@ -12,13 +15,33 @@ contract GuessTheNumberChallenge {
     }
 
     function guess(uint8 n) public payable {
-        require(msg.value == 1 ether);
-        // address payable adr = payable(msg.sender);
+        require(msg.value == 0.1 ether);
+        bool success = false;
 
-        if (n == answer) {
-            payable(msg.sender).transfer(2 ether);
-            // address payablemsg.sender.transfer(2 ether);
+        if (n == ANSWER) {
+            (success, ) = payable(msg.sender).call{value: 0.2 ether}("");
         }
     }
 }
+
+contract Attacker {
+    
+    uint8 constant NUMBER = 42;
+    address payable constant ADDRESSS_WHERE_CHALLENGE_CONTRACT_IS_DEPLOYED = payable(0xc7dd3c70834f2B4742B112f4423bD11e18096df0);
+    GuessTheNumberChallenge challenge = GuessTheNumberChallenge(ADDRESSS_WHERE_CHALLENGE_CONTRACT_IS_DEPLOYED);
+    
+    function guess() public payable {
+        challenge.guess{value: 0.1 ether}(NUMBER);
+    }
+    
+    receive() external payable {}
+}
+
+/*
+Adapted to 0.8 version --->
+  
+  Used call instead of transfer.
+  Used the "constant" and "constructor" keywords.
+  The "address" and "address payable" types can not be converted implicitly. 
+*/
 
