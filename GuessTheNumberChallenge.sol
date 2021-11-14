7aef4f8b90b1309cf015a840c23b835584e398a2
josealonso <jose.alonso.programmer@gmail.com>
2021-11-14 01:31:44 +0100

    Added a withdraw function to get my ethers back.

--- GuessTheNumberChallenge.sol
+++ GuessTheNumberChallenge.sol
@@ -7,7 +7,7 @@ contract GuessTheNumberChallenge {
     uint8 constant ANSWER = 42;
 
     constructor() payable {
-        require(msg.value == 100000000 gwei);
+        require(msg.value == 100000000 gwei);    // equals to 0.1 ethers
     }
 
     function isComplete() public view returns (bool) {
@@ -26,12 +26,20 @@ contract GuessTheNumberChallenge {
 
 contract Attacker {
     
+    address payable constant MY_ROPSTEN_ACCOUNT = payable(0x5f793AbBd751f1Ac5B0F95f3C3D117E5Fd218c41);
     uint8 constant NUMBER = 42;
-    address payable constant ADDRESSS_WHERE_CHALLENGE_CONTRACT_IS_DEPLOYED = payable(0xc7dd3c70834f2B4742B112f4423bD11e18096df0);
+    address payable constant ADDRESSS_WHERE_CHALLENGE_CONTRACT_IS_DEPLOYED = payable(0xF1DDe3178f857B008dAA3578a1B13F592f78EfE5);
+    
     GuessTheNumberChallenge challenge = GuessTheNumberChallenge(ADDRESSS_WHERE_CHALLENGE_CONTRACT_IS_DEPLOYED);
     
     function guess() public payable {
         challenge.guess{value: 0.1 ether}(NUMBER);
+    }
+    
+    // Important: a withdraw function is needed if you want your ethers back
+    function withdrawToMyAccount() public {
+        bool success = false;
+        MY_ROPSTEN_ACCOUNT.transfer(address(this).balance);
     }
     
     receive() external payable {}
@@ -43,5 +51,6 @@ Adapted to 0.8 version --->
   Used call instead of transfer.
   Used the "constant" and "constructor" keywords.
   The "address" and "address payable" types can not be converted implicitly. 
+  
 */
 
